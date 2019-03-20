748730379ef1a2257c919f71fffaedbf6fb141b6
grantposell <grantposell@gmail.com>
2019-03-19 23:10:37 -0500

    Added more project templates and added contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends Component {
     render() {
-        return(
-            <div><h1>Contact Page</h1></div>
+        return (
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Grant Posell</h2>
+                        <img
+                            src={require('../images/profile.jpeg')}
+                            alt="avatar"
+                            style={{ height: '250px' }}
+                        />
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr />
+                        <div className="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}><i className="fa fa-phone-square" aria-hidden="true" />(832) 530-8222</ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}><i className="fa fa-envelope" aria-hidden="true" />grantposell@gmail.com</ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}><i className="fa fa-skype" aria-hidden="true" />MySkypeID</ListItemContent>
+                                </ListItem>
+                            </List>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -2,84 +2,213 @@ import React, { Component } from 'react';
 import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = { activeTab: 0 };
     }
-    toggleCategorgies(){
-        if(this.state.activeTab === 0){
-            return(
+    toggleCategorgies() {
+        if (this.state.activeTab === 0) {
+            return (
                 <div className="projects-grid">
-                {/* Project 1 */}
-                <Card shadow={5} style={{minWidth:'450', margin: 'auto'}}>
-                    <CardTitle style={{color: '#fff', height: '176px',background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover'}}>React Project #1</CardTitle>
-                    <CardText>
-                        Lorem Ipsum
+                    {/* Project 1 */}
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover' }}>React Project #1</CardTitle>
+                        <CardText>
+                            Lorem Ipsum
                     </CardText>
-                    <CardActions border>
-                    <Button colored>GitHub</Button>
-                    <Button colored>LiveDemo</Button>
-                    </CardActions>
-                    <CardMenu style={{color: '#fff'}}>
-                        <IconButton name="share"/>
-                    </CardMenu>
-                </Card>
-                {/* Project 2 */}
-                <Card shadow={5} style={{minWidth:'450', margin: 'auto'}}>
-                    <CardTitle style={{color: '#fff', height: '176px',background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover'}}>React Project #2</CardTitle>
-                    <CardText>
-                        Lorem Ipsum
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    {/* Project 2 */}
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover' }}>React Project #2</CardTitle>
+                        <CardText>
+                            Lorem Ipsum
                     </CardText>
-                    <CardActions border>
-                    <Button colored>GitHub</Button>
-                    <Button colored>LiveDemo</Button>
-                    </CardActions>
-                    <CardMenu style={{color: '#fff'}}>
-                        <IconButton name="share"/>
-                    </CardMenu>
-                </Card>
-                {/* Project 3 */}
-                <Card shadow={5} style={{minWidth:'450', margin: 'auto'}}>
-                    <CardTitle style={{color: '#fff', height: '176px',background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover'}}>React Project #3</CardTitle>
-                    <CardText>
-                        Lorem Ipsum
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    {/* Project 3 */}
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover' }}>React Project #3</CardTitle>
+                        <CardText>
+                            Lorem Ipsum
                     </CardText>
-                    <CardActions border>
-                    <Button colored>GitHub</Button>
-                    <Button colored>LiveDemo</Button>
-                    </CardActions>
-                    <CardMenu style={{color: '#fff'}}>
-                        <IconButton name="share"/>
-                    </CardMenu>
-                </Card>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
                 </div>
             )
-        }else if (this.state.activeTab === 1){
-            return(
-                <div><h1>This is Angular</h1></div>
+        } else if (this.state.activeTab === 1) {
+            return (
+                <div className="projects-grid">
+                    {/* Project 1 */}
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover' }}>Angular Project #1</CardTitle>
+                        <CardText>
+                            Lorem Ipsum
+                    </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    {/* Project 2 */}
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover' }}>Angular Project #2</CardTitle>
+                        <CardText>
+                            Lorem Ipsum
+                    </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    {/* Project 3 */}
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover' }}>Angular Project #3</CardTitle>
+                        <CardText>
+                            Lorem Ipsum
+                    </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </div>
             )
-        }else if (this.state.activeTab === 2){
-            return(
-                <div><h1>This is Vue</h1></div>
+        } else if (this.state.activeTab === 2) {
+            return (
+                <div className="projects-grid">
+                    {/* Project 1 */}
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover' }}>Vue Project #1</CardTitle>
+                        <CardText>
+                            Lorem Ipsum
+                    </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    {/* Project 2 */}
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover' }}>Vue Project #2</CardTitle>
+                        <CardText>
+                            Lorem Ipsum
+                    </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    {/* Project 3 */}
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover' }}>Vue Project #3</CardTitle>
+                        <CardText>
+                            Lorem Ipsum
+                    </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </div>
             )
-        }else if (this.state.activeTab === 3){
-            return(
-                <div><h1>This is MongoDB</h1></div>
+        } else if (this.state.activeTab === 3) {
+            return (
+                <div className="projects-grid">
+                    {/* Project 1 */}
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover' }}>Mongo Project #1</CardTitle>
+                        <CardText>
+                            Lorem Ipsum
+                    </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    {/* Project 2 */}
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover' }}>Mongo Project #2</CardTitle>
+                        <CardText>
+                            Lorem Ipsum
+                    </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    {/* Project 3 */}
+                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://facebook.github.io/create-react-app/img/logo-og.png) center / cover' }}>Mongo Project #3</CardTitle>
+                        <CardText>
+                            Lorem Ipsum
+                    </CardText>
+                        <CardActions border>
+                            <Button colored>GitHub</Button>
+                            <Button colored>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </div>
             )
         }
     }
-    
+
     render() {
-        return(
+        return (
             <div className="category-tabs">
-                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab:tabId})} ripple>
-                <Tab>React</Tab>
-                <Tab>Angular</Tab>
-                <Tab>VueJS</Tab>
-                <Tab>MongoDB</Tab>
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>React</Tab>
+                    <Tab>Angular</Tab>
+                    <Tab>VueJS</Tab>
+                    <Tab>MongoDB</Tab>
                 </Tabs>
 
-                
+
                 <Grid>
                     <Cell col={12}>
                         <div className="content">{this.toggleCategorgies()}</div>
