93df57fa2af2955f35241c57fd1fc9f2dba21fac
grantposell <grantposell@gmail.com>
2019-04-07 22:20:08 -0500

    Changed resume

--- src/components/resumeupload.js
+++ src/components/resumeupload.js
@@ -8,7 +8,7 @@ class ResumeUpload extends Component {
                 <Cell col={10}>
                 </Cell>
                 <Cell col={2}>
-                <a href='./assets/images/Resume.docx' download>
+                <a href='./assets/images/Resume 2019.docx' download>
                 <Button raised ripple colored style={{backgroundColor: '#003973'}}>Download
                 </Button> </a>
                 </Cell>
