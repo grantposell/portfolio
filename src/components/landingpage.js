f316719a7310802b0bbb76df11b57dfb1da9c1c4
grantposell <grantposell@gmail.com>
2020-05-17 14:22:25 -0500

    Added Typed JS

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,35 +1,41 @@
 import React, { Component } from 'react';
+import Typed from 'react-typed';
 import { Grid, Cell } from 'react-mdl';
+
+
 
 class LandingPage extends Component {
     render() {
-        return(
-            <div style ={{width: '100%', margin: 'auto'}}>
+        return (
+            <div style={{ width: '100%', margin: 'auto' }}>
                 <Grid className="landing-grid">
                     <Cell col={12}>
-                    <img 
-                    src={require('../images/profile.jpeg')}
-                    alt="avatar"
-                    className="avatar-img"
-                    />
+                        <img
+                            src={require('../images/profile.jpeg')}
+                            alt="avatar"
+                            className="avatar-img" />
 
-                    <div className="banner-text">
-                    <h1>Full Stack Web Developer</h1>
+                        <div className="banner-text">
+                            <Typed strings={['Product Developer','Full Stack Web Developer','Product Management']} typeSpeed={40} backSpeed={50} attr="placeholder" loop >
+                            <input type="text"/>
+                            </Typed>
 
-                    <hr />
+                            <h1>Full Stack Web Developer</h1>
 
-                    <p>HTML/CSS | JavaScript | React | NodeJS | MongoDB | MySQL | Firebase</p>
-                    <div className="social-links">
-                    {/* LinkedIn */}
-                    <a href="https://www.linkedin.com/in/grantposell/" rel= "noopener noreferrer" target="_blank">
-                    <i className="fa fa-linkedin-square"  aria-hidden="true" />
-                    </a>
-                    {/* Github */}
-                    <a href="https://github.com/grantposell" rel= "noopener noreferrer" target="_blank">
-                    <i className="fa fa-github-square"  aria-hidden="true" />
-                    </a>
-                    </div>
-                    </div>
+                            <hr />
+
+                            <p>HTML/CSS | JavaScript | React | NodeJS | MongoDB | MySQL | Firebase</p>
+                            <div className="social-links">
+                                {/* LinkedIn */}
+                                <a href="https://www.linkedin.com/in/grantposell/" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                </a>
+                                {/* Github */}
+                                <a href="https://github.com/grantposell" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+                            </div>
+                        </div>
                     </Cell>
                 </Grid>
             </div>
