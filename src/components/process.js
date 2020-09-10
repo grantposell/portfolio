4fa3c72797b003827d6e3bdf489b0a8d5dce6aa6
grantposell <grantposell@gmail.com>
2020-09-09 20:46:14 -0500

    Added Research section to process.js

--- src/components/process.js
+++ src/components/process.js
@@ -27,7 +27,30 @@ class Process extends Component {
                     "The Stakeholders" are the people or entity that will be using the product or new addition. Get to know what they like, how they work, and what makes them the most efficient. Stakeholders are often vocal about things that they really like or really dislike similar to The Inceptor but these are the people that are affected the most by the change. Understanding how they work on a daily basis and what they prefer will help in finding the right product to suit their needs. 
                     <hr/>
                     <h3>Research</h3>
-                    
+                    <h4>Preparation</h4>
+                    <p>Researching a product, processes, and new ideas starts with three different questions:</p>
+                    <ul>
+                        <li>Who is the expert or leader in this product, process, or new idea?</li>
+                        <li>How can we utilise what they have done to get to where they are now?</li>
+                        <li>How can we be bettter at the product, process or new idea?</li>
+                    </ul>
+                    <h4>The Art</h4>
+                    <p>
+                    Working in Recruiting and reading The Art of War by Sun Tsu taught me that in order to beat your opponent, one must know the opponent holistically. In order to know your opponent, one must research critical pieces that position you to be in a strategic spot to have the advantage. This is done by understanding how the entity functions, where their practices arise, and what are weak points that can be optimized. </p>
+                    <p>Simply expressed, the Product Manager or entity carrying out the project must first understand what they need to look for in order to find the right materials. Next, find who does it the best. Want to be better at HR or Project Management, look to the Society of Human Resources Managers or the Project Management Institute. They have the experience that has built the best individuals in the industry and they have set products that they highly endorse. These products will then give you the needed information to understand how your product will need to meet and exceed expectations of these other products. Products are made to be the best by building off of solutions that pioneered the way.</p>
+                    <h4>Building the Search String</h4>
+                    <p>Getting all the information you need is pretty easy to do with the advent of Internet 2.0 and the ability to get information at our fingertips. The hard part is to decipher what is needed to build the best product, improve a process or achieve a grand idea.</p>
+                    <p>In recruiting, we always called this "Building the Search String" because we would build up a huge database of terms or definitions then cut it down to get to the right search. In this case, the optimal search would be to make sure you have the right informationt to make the best decisions to make, change or revise a product.</p>
+                    <h4>Criteria</h4>
+                    <p>We have now gathered important information to help build, change or revise a product. We now need to cut down our information by using a method to decipher what is needed. In my past experience, teams have used a Weighted Decision Matrix to allocate values to important facets of a process or product, evaluate the information by assigning them a value from 1-5, and then adding up the values and multiplying by the weighting to then get a total score. At the end of the process, you will know what product, process, or piece of information to use regardless of bias.
+                    </p>
+                    <h3>Selection</h3>
+
+                    <h3>Implementation</h3>
+
+                    <h3>Evaluation</h3>
+
+                    <h3>Improvement or Deprecate</h3>
 
 
                 </Cell>
