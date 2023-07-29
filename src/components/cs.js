bb76d864028892eb2b1d4fe86e4e8903ef1018de
grantposell <grantposell@gmail.com>
2023-07-28 21:57:39 -0500

    Change measurements on landing page

--- src/components/cs.js
+++ src/components/cs.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 
+
 class CS extends Component {
     render() {
         return (
             <Grid className="product-management">
                 <Cell col={12}>
                     <h1 style={{ textAlign: 'center', fontStyle: 'italic' }}>Customer Success Methodology</h1>
+                    <div id="substack-feed-embed"></div>
+
+
+
                 </Cell>
                 </Grid>
             )
